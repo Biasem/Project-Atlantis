8ea0b5cff8dfc414eb79acaade9824e41337032d
jmcamacho <jmcamachosanchez@gmail.com>
2022-09-29 20:49:45 +0200

    No funca el coso de sacar cosas :(

--- src/main/java/com/example/atlantis/controller/MainController.java
+++ src/main/java/com/example/atlantis/controller/MainController.java
@@ -1,18 +1,43 @@
 package com.example.atlantis.controller;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import com.example.atlantis.model.Cliente;
+import com.example.atlantis.service.ClienteService;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class MainController{
 
     @GetMapping("/main")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+    public String main(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
-        return "main";
+        return "greetings";
+    }
+
+    @Autowired
+    private ClienteService clienteService;
+
+    @RequestMapping("/clientes")
+    public List<Cliente> getCliente(){
+        return clienteService.getAll() ;
+    }
+    @RequestMapping("/listaClientes")
+    public ModelAndView listclientes(){
+        List<Cliente> listClientes= clienteService.getAll();
+        ModelAndView model = new ModelAndView("greeting");
+        model.addObject("greeting", listClientes);
+        return model ;
+    }
+
+    @PostMapping("/clientes/save")
+    public void guardarCliente(@RequestBody Cliente humano){
+        Cliente clientefinal = clienteService.getById(humano.getId());
+
     }
 
 }
