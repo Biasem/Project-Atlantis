a367c49d1d82645e73110710eff7a76fb8ba4763
Biasem <rnietourena@safareyes.es>
2022-09-30 17:29:57 +0200

    asegurar

--- src/main/java/com/example/atlantis/controller/MainController.java
+++ src/main/java/com/example/atlantis/controller/MainController.java
@@ -12,6 +12,7 @@ public class MainController{
     @GetMapping("/main")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
+
         return "main";
     }
 
