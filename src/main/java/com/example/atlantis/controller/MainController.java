8d0ea6603275e32276e205d3db2e014ca67f222c
jmcamacho <jmcamachosanchez@gmail.com>
2022-09-30 17:38:39 +0200

    borrando cosas

--- src/main/java/com/example/atlantis/controller/MainController.java
+++ src/main/java/com/example/atlantis/controller/MainController.java
@@ -27,7 +27,7 @@ public class MainController{
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
+        model.addAttribute("cliente" );
         return "greeting";
     }
 }
