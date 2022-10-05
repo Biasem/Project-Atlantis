5e74f77f1dd3a85277bbacd943807dbdedd40b15
Biasem <rnietourena@safareyes.es>
2022-10-05 18:57:27 +0200

    busqueda simple, contains en nombre y localidad

--- src/main/java/com/example/atlantis/controller/MainController.java
+++ src/main/java/com/example/atlantis/controller/MainController.java
@@ -1,6 +1,7 @@
 package com.example.atlantis.controller;
 import com.example.atlantis.model.Busqueda;
 import com.example.atlantis.model.Hotel;
+import com.example.atlantis.service.BusquedaService;
 import com.example.atlantis.service.HotelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,31 +17,46 @@ public class MainController{
     @Autowired
     private HotelService hotelService;
 
-    @RequestMapping("/main")
-    public ModelAndView listaHotel(@ModelAttribute Busqueda busqueda){
-        List<Hotel> listaprimera= hotelService.getAll();
+    @Autowired
+    private BusquedaService busquedaService;
+
+//    @RequestMapping("/main")
+//    public ModelAndView listaHotel(@ModelAttribute Busqueda busqueda){
+//        List<Hotel> listaprimera= hotelService.getAll();
+//        Collections.shuffle(listaprimera);
+//        List<Hotel> listaHotel = listaprimera.subList(0, 3);
+//        ModelAndView model = new ModelAndView("main");
+//        model.addObject("listaHotel", listaHotel);
+//
+//
+//        model.addObject("busqueda", busqueda);
+//
+//        busquedaService.AccionBuscar(busqueda.getHotelBuscar());
+//
+//        //System.out.println(busqueda.getFechaInicial());
+//        //System.out.println(busqueda.getFechaFinal());
+//        //System.out.println(busqueda.getNumHuespedes());
+//
+//        return model ;
+//    }
+
+    @GetMapping("/main")
+    public ModelAndView listaHotel(@ModelAttribute Busqueda busqueda) {
+        List<Hotel> listaprimera = hotelService.getAll();
         Collections.shuffle(listaprimera);
         List<Hotel> listaHotel = listaprimera.subList(0, 3);
         ModelAndView model = new ModelAndView("main");
         model.addObject("listaHotel", listaHotel);
+    return model ;
+    }
 
-        model.addObject("busqueda", new Busqueda());
+    @PostMapping("/main")
+    public ModelAndView listaHotelPost(@ModelAttribute Busqueda busqueda) {
+        List<Hotel> listaHoteles = hotelService.getAll();
+        ModelAndView model = new ModelAndView("main");
         model.addObject("busqueda", busqueda);
-
-        //System.out.println(busqueda.getHotelBuscar());
-        //System.out.println(busqueda.getFechaInicial());
-        //System.out.println(busqueda.getFechaFinal());
-        //System.out.println(busqueda.getNumHuespedes());
-
-
-
-
+        busquedaService.AccionBuscar(busqueda.getHotelBuscar(),listaHoteles);
         return model ;
     }
 
-    @GetMapping("/login")
-    public String login(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "sesion";
-    }
 }
--- src/main/java/com/example/atlantis/service/BusquedaService.java
+++ src/main/java/com/example/atlantis/service/BusquedaService.java
@@ -1,4 +1,29 @@
 package com.example.atlantis.service;
+import com.example.atlantis.model.Hotel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class BusquedaService {
+
+
+
+    public List<Hotel> AccionBuscar(String cadenaABuscar, List<Hotel> listaHoteles){
+        List<Hotel> coincidencias = new ArrayList<>();
+        cadenaABuscar = cadenaABuscar.toLowerCase();
+
+        for( Hotel hotel:listaHoteles){
+            if((hotel.getLocalidad().toLowerCase().contains(cadenaABuscar))||
+                    (hotel.getNombre().toLowerCase().contains(cadenaABuscar))){
+                coincidencias.add(hotel);
+            }
+        }
+
+       //coincidencias.forEach(hotel -> System.out.println(hotel.getNombre()));
+        return coincidencias;
+    }
+
 }
