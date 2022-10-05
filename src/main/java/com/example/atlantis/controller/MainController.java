0778db713ade8ffc849917bbcb010887708f9318
jmcamacho <jmcamachosanchez@gmail.com>
2022-10-05 17:14:13 +0200

    HTML actualizado

--- src/main/java/com/example/atlantis/controller/MainController.java
+++ src/main/java/com/example/atlantis/controller/MainController.java
@@ -5,12 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import com.example.atlantis.model.Cliente;
-import com.example.atlantis.service.ClienteService;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
@@ -27,7 +23,7 @@ public class MainController{
         List<Hotel> listaHotel = listaprimera.subList(0, 3);
         ModelAndView model = new ModelAndView("main");
         model.addObject("listaHotel", listaHotel);
-        return model ;
+        return model;
     }
 
     @GetMapping("/login")
