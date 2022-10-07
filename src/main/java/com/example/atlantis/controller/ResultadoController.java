e7f059f87c70d841a6a03aa8554b6939f13d6921
jmcamacho <jmcamachosanchez@gmail.com>
2022-10-07 17:30:29 +0200

    Muestra resultados de busqueda

--- src/main/java/com/example/atlantis/controller/ResultadoController.java
+++ src/main/java/com/example/atlantis/controller/ResultadoController.java
@@ -9,35 +9,34 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-
 @Controller
-public class MainController{
+public class ResultadoController{
+
     @Autowired
     private HotelService hotelService;
 
     @Autowired
     private BusquedaService busquedaService;
 
-    @GetMapping("/main")
+    @GetMapping("/resultado")
     public ModelAndView listaHotel(@ModelAttribute Busqueda busqueda) {
         List<Hotel> listaprimera = hotelService.getAll();
         Collections.shuffle(listaprimera);
         List<Hotel> listaHotel = listaprimera.subList(0, 3);
-        ModelAndView model = new ModelAndView("main");
+        ModelAndView model = new ModelAndView("resultado");
         model.addObject("listaHotel", listaHotel);
         return model;
     }
 
-    @PostMapping("/main")
-    public ModelAndView listaHoteles(@ModelAttribute Busqueda busqueda) {
+    @PostMapping("/resultado")
+    public ModelAndView listaHotelPost(@ModelAttribute Busqueda busqueda) {
         List<Hotel> listaHoteles = hotelService.getAll();
         ModelAndView model = new ModelAndView("resultado");
-        List<Hotel> filtro = busquedaService.AccionBuscar(busqueda,listaHoteles);
-        model.addObject("filtro", filtro);
+        model.addObject("busqueda", busqueda);
+        busquedaService.AccionBuscar(busqueda,listaHoteles);
         return model ;
     }
 }
---
+++ src/main/java/com/example/atlantis/model/ListaModelo.java
@@ -0,0 +1,8 @@
+package com.example.atlantis.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaModelo {
+    static List<Hotel> local = new ArrayList<>();
+}
