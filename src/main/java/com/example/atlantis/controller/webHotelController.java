066c521657564591d60907f3bc3b6fd0000d6305
jmcamacho <jmcamachosanchez@gmail.com>
2022-10-09 19:59:41 +0200

    comienzo de mostrar los hoteles

--- src/main/java/com/example/atlantis/controller/webHotelController.java
+++ src/main/java/com/example/atlantis/controller/webHotelController.java
@@ -1,11 +1,11 @@
 package com.example.atlantis.controller;
+import com.example.atlantis.model.BuscadorID;
 import com.example.atlantis.model.Busqueda;
 import com.example.atlantis.model.Hotel;
 import com.example.atlantis.service.BusquedaService;
 import com.example.atlantis.service.HotelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -13,7 +13,7 @@ import java.util.Collections;
 import java.util.List;
 
 @Controller
-public class ResultadoController{
+public class webHotelController {
 
     @Autowired
     private HotelService hotelService;
@@ -21,13 +21,12 @@ public class ResultadoController{
     @Autowired
     private BusquedaService busquedaService;
 
-    @GetMapping("/resultado")
-    public ModelAndView listaHotel(@ModelAttribute Busqueda busqueda) {
-        List<Hotel> listaprimera = hotelService.getAll();
-        Collections.shuffle(listaprimera);
-        List<Hotel> listaHotel = listaprimera.subList(0, 3);
-        ModelAndView model = new ModelAndView("resultado");
-        model.addObject("listaHotel", listaHotel);
+    @GetMapping("/hotel")
+    public ModelAndView resultadoHotel(@ModelAttribute BuscadorID numero) {
+        Hotel resultado = new Hotel();
+        resultado = hotelService.getById(numero.getNumID());
+        ModelAndView model = new ModelAndView("webHotel");
+        model.addObject("resultado", resultado);
         return model;
     }
 
