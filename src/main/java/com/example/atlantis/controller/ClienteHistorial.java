185b694cf244db74f9bf8b41e97ff3e433afa1a8
Antonito <anunezmorejon@safareyes.es>
2022-10-18 20:14:58 +0200

    Historia Reserva Cli solo falta HTML 18/10

--- src/main/java/com/example/atlantis/controller/ClienteHistorial.java
+++ src/main/java/com/example/atlantis/controller/ClienteHistorial.java
@@ -2,14 +2,23 @@ package com.example.atlantis.controller;
 
 
 import com.example.atlantis.model.Cliente;
-import com.example.atlantis.model.Login;
+import com.example.atlantis.model.Reserva;
+import com.example.atlantis.service.ClienteService;
+import com.example.atlantis.service.ReservaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class ClienteHistorial {
+
+    @Autowired
+    private ReservaService reservaService;
 
     @GetMapping("/historialReservaCliente")
     public ModelAndView historial(@ModelAttribute Cliente cliente) {
@@ -18,6 +27,20 @@ public class ClienteHistorial {
         return model;
 
     }
+
+    @PostMapping("/historialReservaCliente")
+    public ModelAndView historial2(@ModelAttribute Cliente cliente) {
+
+        ModelAndView model = new ModelAndView("hotelWeb");
+        List<Reserva> todas = reservaService.todasReservas(cliente.getId());
+
+        model.addObject("todas", todas);
+        System.out.println(todas);
+
+
+        return model;
+    }
+
 
 
 }
