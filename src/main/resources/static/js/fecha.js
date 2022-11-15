c3220f67e978f853af8743ba12f4c0d71eec6619
Biasem <rnietourena@safareyes.es>
2022-11-15 20:11:40 +0100

    funciona javascript html dates

--- src/main/resources/static/js/fecha.js
+++ src/main/resources/static/js/fecha.js
@@ -1,9 +1,4 @@
-function alerta() {
-    alert("prueba")
-}
-
 function cambiofecha(){
     var fecha = document.getElementById("fecha_inicio").value
     document.getElementById("fecha_final").setAttribute("min",fecha)
-console.log(document.getElementById("fecha_final"));
 }
