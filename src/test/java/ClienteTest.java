0f4ae3ae476aa9e802913206d1e7eb7e751d2d7b
Biasem <rnietourena@safareyes.es>
2022-11-22 20:02:59 +0100

    test cliente

--- src/test/java/ClienteTest.java
+++ src/test/java/ClienteTest.java
@@ -1,27 +1,55 @@
 import com.example.atlantis.model.Cliente;
 import com.example.atlantis.model.Login;
 import com.example.atlantis.model.Rol;
+import com.example.atlantis.repository.ClienteRepository;
+import com.example.atlantis.service.ClienteService;
 import com.github.javafaker.Faker;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import static org.springframework.test.util.AssertionErrors.assertEquals;
+import static org.springframework.test.util.AssertionErrors.assertNotNull;
 
 public class ClienteTest {
+    @InjectMocks
+    ClienteService clienteService;
+    @Mock
+    ClienteRepository clienteRepository;
+
 
     private static Faker faker = new Faker();
     private Rol rol;
-
-    @Test
-    public void guardadoClienteBBDDTest(){
-
+@Test
+    public Cliente crearCliente(){
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         Login login = new Login();
         Cliente cliente = new Cliente();
         login.setEmail(faker.internet().emailAddress());
-        login.setPassword("1234");
+        login.setPassword(passwordEncoder.encode("1234"));
         login.setRol(rol.CLIENTE);
         cliente.setEmail(login);
         cliente.setApellidos(faker.name().lastName());
         cliente.setNombre(faker.name().name());
         cliente.setPais(faker.country().name());
-        cliente.setDni(""+faker.number().numberBetween( 10000000, 99999999)+faker.);
-
+        cliente.setDni(""+faker.phoneNumber().subscriberNumber(8)+
+                faker.letterify("?"));
+        cliente.setTelefono(faker.phoneNumber().subscriberNumber(9));
+        return cliente;
     }
+
+    @Test
+    public void guardarClienteBBDD(){
+
+
+        Cliente clienteEsperado = crearCliente();
+
+        Cliente clienteObtenido =clienteRepository.save(clienteEsperado);
+
+        assertNotNull("No se ha guardado en BBDD", clienteObtenido);
+        assertEquals("Los productos no coinciden",clienteEsperado.getDni(),clienteObtenido.getDni());
+    }
+
+
 }
